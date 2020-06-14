38ffe44412a7c4b844e2b283a01fa85f5a40cdf2
tondi <mtondos2@gmail.com>
2020-06-14 19:10:14 +0200

    refactor

--- src/pl/edu/agh/automatedgrader/jtp2/lab3/impl/DefaultMain.java
+++ src/pl/edu/agh/automatedgrader/jtp2/lab3/impl/DefaultMain.java
@@ -7,20 +7,25 @@ import java.util.List;
 
 public class DefaultMain implements Main {
     private static int PHILOSOPHERS_COUNT = 5;
+    private List<DefaultPhilosopher> philosophers = new ArrayList<>();
+    private List<DefaultFork> forks = new ArrayList<>();
 
     public static void main(String[] args) {
-        List<DefaultPhilosopher> philosophers = new ArrayList<>();
-        List<Object> forks = new ArrayList<>();
+        DefaultMain df = new DefaultMain();
+        df.eatingThinking(5, 5, 100, 100);
+    }
 
-        for (int i = 0; i < PHILOSOPHERS_COUNT; i++) {
-            forks.add(new Object());
+    @Override
+    public void eatingThinking(int howMany, int numberOfPhilosophers, int maxTimeForEating, int maxTimeForThinking) {
+        for (int i = 0; i < numberOfPhilosophers; i++) {
+            forks.add(new DefaultFork());
         }
 
-        for (int i = 0; i < PHILOSOPHERS_COUNT; i++) {
+        for (int i = 0; i < numberOfPhilosophers; i++) {
             Object leftFork = forks.get(i);
-            Object rightFork = forks.get((i + 1) % PHILOSOPHERS_COUNT);
+            Object rightFork = forks.get((i + 1) % numberOfPhilosophers);
 
-            if(i == PHILOSOPHERS_COUNT - 1)
+            if(i == numberOfPhilosophers - 1)
                 philosophers.add(new DefaultPhilosopher(leftFork, rightFork));
             else
                 philosophers.add(new DefaultPhilosopher(rightFork, leftFork));
@@ -31,18 +36,13 @@ public class DefaultMain implements Main {
     }
 
     @Override
-    public void eatingThinking(int howMany, int numberOfPhilosophers, int maxTimeForEating, int maxTimeForThinking) {
-
-    }
-
-    @Override
     public List<DefaultFork> getForks() {
-        return null;
+        return forks;
     }
 
     @Override
     public List<DefaultPhilosopher> getPhilosophers() {
-        return null;
+        return philosophers;
     }
 
     @Override
